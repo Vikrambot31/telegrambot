68a05d0d2b093d4ffb42e224b355f4aa832a9f7e
Vikram <vikramsave@ukr.net>
2025-04-05 13:24:00 +0300

    12

--- hd_site_form/app.py
+++ hd_site_form/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
-from urllib.parse import quote_plus
+from flask import Flask, render_template, request, redirect
+from urllib.parse import urlencode
 
 app = Flask(__name__)
 
@@ -7,31 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/form')
-def form():
-    return render_template('form.html')
-
-@app.route('/submit', methods=['POST'])
-def submit():
+@app.route('/result', methods=['POST'])
+def result():
     name = request.form['name']
-    birth_date = request.form['birth_date']
-    birth_time = request.form['birth_time']
-    birth_place = request.form['birth_place']
+    birthdate = request.form['birthdate']
+    birthtime = request.form['birthtime']
+    birthcity = request.form['birthcity']
 
-    # Формируем ссылку с параметрами (кодировка параметров через quote_plus)
-    url = (
-        "https://app.maiamechanics.com/#/free-chart?"
-        f"name={quote_plus(name)}&"
-        f"birth_date={quote_plus(birth_date)}&"
-        f"birth_time={quote_plus(birth_time)}&"
-        f"birth_place={quote_plus(birth_place)}"
-    )
+    # Формируем ссылку на сервис бодиграфа
+    query_params = urlencode({
+        'name': name,
+        'birthdate': birthdate,
+        'birthtime': birthtime,
+        'birthcity': birthcity
+    })
 
-    return render_template('result.html', chart_url=url)
+    link = f"https://app.maiamechanics.com/#/free-chart?{query_params}"
 
-@app.route('/success')
-def success():
-    return render_template('success.html')
+    return render_template('result.html', name=name, birthdate=birthdate, birthtime=birthtime, birthcity=birthcity, link=link)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
